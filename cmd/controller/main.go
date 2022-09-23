0f266ff4ec20e04a59089e3feb53018039628404
Sri Panyam <sri.panyam@gmail.com>
2022-09-23 01:21:15 -0700

    Adding stub for control svc bin

---
+++ cmd/controller/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"github.com/panyam/slicer/service"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+var (
+	addr = flag.String("addr", "localhost:7000", "Address to run the echo service on.")
+)
+
+func main() {
+	grpcServer := grpc.NewServer(
+	// grpc.StreamInterceptor(grpc_prometheus.StreamServerInterceptor),
+	// grpc.UnaryInterceptor(grpc_prometheus.UnaryServerInterceptor),
+	)
+	service.RegisterControlServiceServer(grpcServer, service.NewControlService())
+	// grpc_prometheus.Register(grpcServer)
+	log.Printf("Initializing Control Server on %s", *addr)
+	lis, err := net.Listen("tcp", *addr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer.Serve(lis)
+}
